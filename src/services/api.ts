2c03d78fd5cdb44be110695a6a5f0d569a901979
Abu Nabeelah <abunabeelah@Abus-MacBook-Air.local>
2026-06-15 13:04:59 +0100

    chore: force standalone build and cloud api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-const API_URL =
-  process.env.EXPO_PUBLIC_API_URL ||
-  (__DEV__ ? "http://localhost:4000" : "https://soul-link-api.onrender.com");
+// const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:4000";
+// const API_URL = "https://soul-link-api.onrender.com";
+
+const API_URL = __DEV__
+  ? "http://172.29.118.139:4000"
+  : "https://soul-link-api.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
